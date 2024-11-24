7b6ff408daa7f546ab2629ac93cfb898e30f317a
GOLDEN0502 <123xcw@gmail.com>
2024-11-24 16:11:41 +0300

    Password_Generator

---
+++ PaswordGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿Random random = new Random();
+int intPasword = random.Next();
+int intPasword2 = random.Next(1,2);
+char[] letters = new char[9];
+for (int i = 0;i < 9 ; i++)
+{
+    letters[i] = (char)('A' + random.Next(26)); // Заполняем массив
+
+
+}
+if (intPasword2 == 1)
+{
+    Console.WriteLine("Сгенерированный пароль: " + new string(letters) + intPasword);
+}
+if (intPasword2 == 2)
+{
+    Console.WriteLine("Сгенерированный пароль: " + intPasword + new string(letters));
+}
